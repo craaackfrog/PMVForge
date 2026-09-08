c6372fcb75111deea4e65f35cc504fe571d7a97e
craaackfrog <trissolaris@proton.me>
2026-09-08 20:50:21 -0300

    chore: sync frontend version

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -4,7 +4,7 @@
  */
 export const APP_NAME = 'PMVForge'
 export const APP_SLUG = 'pmvforge'
-export const APP_VERSION = '0.1.0'
+export const APP_VERSION = '0.2.0'
 export const APP_DESCRIPTION = 'All-in-one local tool for creating PMVs from beatmaps'
 
 export const API_BASE = '/api'
